31cd88f35ca20705e0c347eead63538a97f946fe
bramara_ <bramara.sambada.17@gmail.com>
2022-06-26 14:17:28 +0700

    Membuat Test Script berdasarkan video eduwork

--- cypress/e2e/0-personal-learn/first.test.cy.js
+++ cypress/e2e/0-personal-learn/first.test.cy.js
@@ -1,10 +1,26 @@
 /// <reference types="cypress" />
 
 describe ('My First Test',() =>{
-    it('Visit the Kitchen Sink', () => {
+    //coba tutor video eduwork
+    // it('Visit the Kitchen Sink', () => {
+    //     cy.visit('https://example.cypress.io')
+    //     cy.get('h1').contains('Kitchen Sink')
+    //     cy.contains('get').click()
+    //     cy.url().should('include','/commands/querying')
+    // });
+
+    //Tugas 2 - Section 5 Test Script Cypress
+    it('clicking "type" shows the right headings', () => {
         cy.visit('https://example.cypress.io')
-        cy.get('h1').contains('Kitchen Sink')
-        cy.contains('get').click()
-        cy.url().should('include','/commands/querying')
+
+        // cy.pause()
+
+        cy.contains('type').click()
+
+        cy.url().should('include','/commands/actions')
+
+        cy.get('.action-email')
+        .type('bram@mail.id')
+        .should('have.value','bram@mail.id')
     });
 });
