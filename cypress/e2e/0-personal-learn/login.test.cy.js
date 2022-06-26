f4bdf86a2b8e9d8f093fed396ff9ab3623230965
bramara_ <bramara.sambada.17@gmail.com>
2022-06-26 17:06:15 +0700

    Menambah fixture

--- cypress/e2e/0-personal-learn/login.test.cy.js
+++ cypress/e2e/0-personal-learn/login.test.cy.js
@@ -17,4 +17,21 @@ describe ('Working with inputs',() =>{
         // cy.get('#user_password').clear()
         // cy.get('#user_password').type('password')
     });
+    it('Should try to login', () => {
+        cy.fixture("user").then(user =>{
+            const username = user.username
+            const password = user.password
+
+            cy.get('#user_login').clear()
+            cy.get('#user_login').type(username)
+
+            cy.get('input[name="user_password"]').clear()
+            cy.get('input[name="user_password"]').type(password)
+
+            cy.get('input[name="submit"]').click()
+
+            // cy.get('.alert-error').should('have.text','\n                    Login and/or password are wrong.\n                ') ini berhasil
+            cy.get('.alert-error').should('contain.text','Login and/or password are wrong.')
+        })
+    });
 });
