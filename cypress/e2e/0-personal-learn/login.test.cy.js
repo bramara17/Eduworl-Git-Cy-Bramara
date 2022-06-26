53ced42e3657453c15806f4214f048b61946e3eb
bramara_ <bramara.sambada.17@gmail.com>
2022-06-26 19:17:09 +0700

    Custom Commands login

--- cypress/e2e/0-personal-learn/login.test.cy.js
+++ cypress/e2e/0-personal-learn/login.test.cy.js
@@ -21,17 +21,8 @@ describe ('Working with inputs',() =>{
         cy.fixture("user").then(user =>{
             const username = user.username
             const password = user.password
-
-            cy.get('#user_login').clear()
-            cy.get('#user_login').type(username)
-
-            cy.get('input[name="user_password"]').clear()
-            cy.get('input[name="user_password"]').type(password)
-
-            cy.get('input[name="submit"]').click()
-
-            // cy.get('.alert-error').should('have.text','\n                    Login and/or password are wrong.\n                ') ini berhasil
-            cy.get('.alert-error').should('contain.text','Login and/or password are wrong.')
+            cy.login(username,password)
+            //cy.get('.alert-error').should('contain.text','Login and/or password are wrong.')
         })
     });
 });
